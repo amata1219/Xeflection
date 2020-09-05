a0ceba9a3900cf3a74429fd92a561baecb99f37d
amata1219 <suzukiamata@gmail.com>
2020-09-06 03:17:43 +0900

    リフレクションされたオブジェクトのメソッドとコンストラクタの呼出に対応

--- src/amata1219/xeflection/AnyReflected.scala
+++ src/amata1219/xeflection/AnyReflected.scala
@@ -1,8 +1,14 @@
 package amata1219.xeflection
 
-import java.lang.reflect.Field
+import java.lang.reflect.{Constructor, Field, Method}
 
 class AnyReflected(val clazz: Class[_], val instance: Any) {
+
+  object AnyReflected {
+
+    val NONE: AnyReflected = Reflect.on(().getClass, ())
+
+  }
 
   def get[T](): T = instance.asInstanceOf[T]
 
@@ -22,6 +28,32 @@ class AnyReflected(val clazz: Class[_], val instance: Any) {
     val field: Field = clazz.getDeclaredField(name)
     field.setAccessible(true)
     field
+  }
+
+  def call(name: String, args: Any*): AnyReflected = {
+    val types: Array[Class[_]] = args.map(_.getClass).toArray
+    accessibleMethod(name, types:_*).invoke(instance, args) match {
+      case null => AnyReflected.NONE
+      case result => Reflect.on(result.getClass, result)
+    }
+  }
+
+  private def accessibleMethod(name: String, types: Class[_]*): Method = {
+    val method: Method = clazz.getDeclaredMethod(name, types:_*)
+    method.setAccessible(true)
+    method
+  }
+
+  def create(args: Any*): AnyReflected = {
+    val types: Array[Class[_]] = args.map(_.getClass).toArray
+    val result = accessibleConstructor(types:_*).newInstance(args)
+    Reflect.on(clazz, result)
+  }
+
+  private def accessibleConstructor(types: Class[_]*): Constructor[_] = {
+    val constructor: Constructor[_] = clazz.getDeclaredConstructor(types:_*)
+    constructor.setAccessible(true)
+    constructor
   }
 
 
