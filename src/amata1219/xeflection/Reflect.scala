1c27f4e9ee57d8de451551dbf361a74c3d88f82a
amata1219 <suzukiamata@gmail.com>
2020-09-20 15:55:23 +0900

    こみっとし祭

--- src/amata1219/xeflection/Reflect.scala
+++ src/amata1219/xeflection/Reflect.scala
@@ -2,14 +2,10 @@ package amata1219.xeflection
 
 object Reflect {
 
-  def on(instance: Any): AnyReflected = on(instance.getClass, instance)
-
-  def on(clazzName: String, instance: Any): AnyReflected = on(Class.forName(clazzName), instance)
-
-  def on(clazz: Class[_], instance: Any): AnyReflected = new AnyReflected(clazz, instance)
-
-  def onNetMinecraftServer(simpleClazzName: String, instance: Any): AnyReflected = on(s"${NetMinecraftServer.path}.$simpleClazzName", instance)
-
-  def onOrgBukkitCraftbukkit(simpleClazzName: String, instance: Any): AnyReflected = on(s"${OrgBukkitCraftbukkit.path}.$simpleClazzName", instance)
+  def on(target: Any): AnyReflected = target match {
+    case clazz: Class[_] => new AnyReflected(clazz, null)
+    case clazzName: String => new AnyReflected(Class.forName(clazzName), null)
+    case instance => new AnyReflected(null, instance)
+  }
 
 }
